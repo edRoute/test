145f3e53ef8c3fd2920b66e46d2767bb67aa0304
DevOps <devops@macbuildbox.local>
2017-08-22 19:51:55 -0400

    asdf

--- ServerTest/AppDelegate.swift
+++ ServerTest/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        print("FINALLY")
+        print("FINdALLY")
         return true
     }
     
